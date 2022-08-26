ea02034ba42b2410862789144ff3c42d8a89a517
Simon Zelenski <simon.zelenski@gmail.com>
2022-08-27 01:06:46 +0200

    add print

--- practice/family.go
+++ practice/family.go
@@ -1,6 +1,18 @@
 package practice
 
+import (
+	"fmt"
+)
+
 type Person struct {
 	Name string
 	Age  int
+}
+
+func PrintPerson(p *Person) {
+	fmt.Printf("name: %s; age: %d", p.Name, p.Age)
+}
+
+func PrintPerson2(p Person) {
+	fmt.Printf("name: %s; age: %d", p.Name, p.Age)
 }
